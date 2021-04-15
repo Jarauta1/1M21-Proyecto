23e83fd2433903f58a1549b3f20bba5044ee7638
Jarauta1 <diegojarauta8@gmail.com>
2021-04-15 10:26:21 +0200

    Update API num ligas

--- JS/index.js
+++ JS/index.js
@@ -15,38 +15,38 @@ function entrar(country) { //El string recibido se almacena en la variable "coun
             if (country == "Spain") {
                 location.href = './league.html' //Página a la que nos lleva la función
                 localStorage.setItem("country", country) //Se guarda en local el nombre del país para posterior utilización
-                localStorage.setItem("league", datos[17].Competitions[0].Name) //Se guarda en local el nombre de la liga (dado por la API) para posterior utilización
-                localStorage.setItem("CompetitionId", datos[17].Competitions[0].CompetitionId) //Se guarda en local la ID de la liga (dado por la API) para poder incluirlo posteriormente en enlaces para llamadas a la API
+                localStorage.setItem("league", datos[20].Competitions[0].Name) //Se guarda en local el nombre de la liga (dado por la API) para posterior utilización
+                localStorage.setItem("CompetitionId", datos[20].Competitions[0].CompetitionId) //Se guarda en local la ID de la liga (dado por la API) para poder incluirlo posteriormente en enlaces para llamadas a la API
             }
             else if (country == "England") {
-                location.href = './league.html'
-                localStorage.setItem("country", country)
-                localStorage.setItem("league", datos[9].Competitions[0].Name)
-                localStorage.setItem("CompetitionId", datos[9].Competitions[0].CompetitionId)
-            }
-            else if (country == "France") {
-                location.href = './league.html'
-                localStorage.setItem("country", country)
-                localStorage.setItem("league", datos[10].Competitions[0].Name)
-                localStorage.setItem("CompetitionId", datos[10].Competitions[0].CompetitionId)
-            }
-            else if (country == "Italy") {
-                location.href = './league.html'
-                localStorage.setItem("country", country)
-                localStorage.setItem("league", datos[13].Competitions[0].Name)
-                localStorage.setItem("CompetitionId", datos[13].Competitions[0].CompetitionId)
-            }
-            else if (country == "Germany") {
                 location.href = './league.html'
                 localStorage.setItem("country", country)
                 localStorage.setItem("league", datos[11].Competitions[0].Name)
                 localStorage.setItem("CompetitionId", datos[11].Competitions[0].CompetitionId)
             }
+            else if (country == "France") {
+                location.href = './league.html'
+                localStorage.setItem("country", country)
+                localStorage.setItem("league", datos[12].Competitions[0].Name)
+                localStorage.setItem("CompetitionId", datos[12].Competitions[0].CompetitionId)
+            }
+            else if (country == "Italy") {
+                location.href = './league.html'
+                localStorage.setItem("country", country)
+                localStorage.setItem("league", datos[16].Competitions[0].Name)
+                localStorage.setItem("CompetitionId", datos[16].Competitions[0].CompetitionId)
+            }
+            else if (country == "Germany") {
+                location.href = './league.html'
+                localStorage.setItem("country", country)
+                localStorage.setItem("league", datos[13].Competitions[0].Name)
+                localStorage.setItem("CompetitionId", datos[13].Competitions[0].CompetitionId)
+            }
             else if (country == "Netherland") {
                 location.href = './league.html'
                 localStorage.setItem("country", country)
-                localStorage.setItem("league", datos[15].Competitions[0].Name)
-                localStorage.setItem("CompetitionId", datos[15].Competitions[0].CompetitionId)
+                localStorage.setItem("league", datos[18].Competitions[0].Name)
+                localStorage.setItem("CompetitionId", datos[18].Competitions[0].CompetitionId)
             }
         }
     })
